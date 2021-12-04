0526a2baf2aa766b5d7b5e5ac087e768f4321bb2
Rahul-D78 <rahulparida933@gmail.com>
2021-12-04 22:16:49 +0530

    basic server created
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('HI')
+})
+
+app.listen(PORT, () => console.log(`server is listening on port 3000`))
