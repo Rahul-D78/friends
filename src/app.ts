e7cdfbcdc6dff678c00a8efc366848ab8b4f89d5
Rahul-D78 <rahulparida933@gmail.com>
2021-12-05 15:37:51 +0530

    database connected
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { createConnection } from 'typeorm';
+import { User } from './models/user';
 import { allRoutes } from './controller/allRoutes';
 const app = express();
 
@@ -6,4 +8,18 @@ app.use(allRoutes)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`server is listening on port 3000`))
+async function start() {
+    await createConnection({
+        type: 'postgres',
+        username: 'demo',
+        password: 'pass',
+        database: 'demo',
+        synchronize: true,
+        entities: [User],
+        logging: true,
+        logger: 'advanced-console'
+    })
+    app.listen(PORT, () => console.log(`server is listening on port 3000`))
+}
+
+start()
